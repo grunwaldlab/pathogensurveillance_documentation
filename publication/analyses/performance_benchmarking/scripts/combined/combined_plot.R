2036941f30b3c04ab922ce4991484ff21423e700
Zachary S.L. Foster <zacharyfoster1989@gmail.com>
2025-08-21 22:03:03 -0700

    feat: add organism type classification to benchmarking data

--- publication/analyses/performance_benchmarking/scripts/combined/combined_plot.R
+++ publication/analyses/performance_benchmarking/scripts/combined/combined_plot.R
@@ -4,6 +4,26 @@ library(readr)
 genome_size_data <- read_csv('results/genome_size/performance_stats_genome_size.csv')
 sample_size_data <- read_csv('results/sample_size/performance_stats_sample_size.csv')
 
+# Add organisms type column
+organism_type <- c(
+  a_thaliana = "eukaryote",
+  b_japonicum = "prokaryote",
+  c_elegans = "eukaryote", 
+  c_sativus = "eukaryote",
+  f_oxysporum = "eukaryote",
+  l_fermentum = "prokaryote",
+  o_tauri = "eukaryote",
+  p_aeruginosa = "prokaryote",
+  p_capsici = "eukaryote",
+  p_ramorum = "eukaryote",
+  p_vivax = "eukaryote",
+  s_olivochromogenes = "prokaryote",
+  u_maydis = "eukaryote",
+  x_hortorum = "prokaryote"
+)
+genome_size_data$organism <- organism_type[genome_size_data$value]
+sample_size_data$organism <- 'prokaryote'
+
 # Make same format
 colnames(genome_size_data)[2] <- 'replicate'
 genome_size_data$replicate <- letters[genome_size_data$replicate]
@@ -14,4 +34,4 @@ sample_size_data$factor <- 'Sample size'
 stopifnot(all(colnames(sample_size_data) == colnames(genome_size_data)))
 combined_data <- rbind(sample_size_data, genome_size_data)
 
-# Plot
+# Make 4 panel dotplot with regression lines using ggplot facets with rows for max_rss_gb (labled "Max RAM") and clock_hours (Labeled "Run time") and columns for genome size and sample size. Color dots and regressions lines by organism (labeled "Organism") AI!
